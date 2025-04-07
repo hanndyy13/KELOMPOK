143320f75e16ed28f1b2fad4b22e467575fba0ae
hannndii <shiirogami95@gmail.com>
2025-04-07 10:51:41 +0700

    menambahkan class KuliahMahasiswa103022300064

--- tpmodul7_kelompok_05/tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/tpmodul7_kelompok_05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 public class DataMahasiswa103022300064
@@ -31,10 +32,46 @@ public class DataMahasiswa103022300064
     }
 }
 
+public class KuliahMahasiswa103022300064
+{
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CourseList
+    {
+        public List<Course> courses { get; set; }
+    }
+
+    public static void readJSON()
+    {
+        string path = "D:\\My Code\\GUI C#\\KELOMPOK\\tpmodul7_kelompok_05\\tpmodul7_kelompok_05\\tp7_2_103022300064.json";
+        string jsonString = File.ReadAllText(path);
+        CourseList? data = JsonSerializer.Deserialize<CourseList>(jsonString);
+
+        if (data != null && data.courses != null)
+        {
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int index = 1;
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine($"MK {index} {course.code} - {course.name}");
+                index++;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Gagal melakukan deserialisasi JSON.");
+        }
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
         DataMahasiswa103022300064.readJSON();
+        KuliahMahasiswa103022300064.readJSON();
     }
 }
