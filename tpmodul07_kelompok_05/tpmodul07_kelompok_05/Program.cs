1462974cbf95cb71569c9e763791534e1b1e9754
muthiazzh <muthiahazzahra200@gmail.com>
2025-04-07 13:53:15 +0700

    menambahkan class DataMahasiswa103022330117

--- tpmodul07_kelompok_05/tpmodul07_kelompok_05/Program.cs
+++ tpmodul07_kelompok_05/tpmodul07_kelompok_05/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul07_kelompok_05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataMahasiswa103022330117.ReadJSON();
+
+
+    }
+}
--- tpmodul07_kelompok_05/tpmodul07_kelompok_05/obj/Debug/net8.0/tpmodul07_kelompok_05.AssemblyInfo.cs
+++ tpmodul07_kelompok_05/tpmodul07_kelompok_05/obj/Debug/net8.0/tpmodul07_kelompok_05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul07_kelompok_05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea645ce654a816b30727bf3b51939064646b4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dbebb39c8092a1caa635cbfca9f0a687a9d1ba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul07_kelompok_05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul07_kelompok_05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
